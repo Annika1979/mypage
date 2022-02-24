81da294e1db53181008a7ae0fb3ddba7c4ff0b09
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-02-24 22:47:23 +0100

    Initial push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // kopplar på sig på url i webläsare. ser till att vi kan hålla koll på vilken url vi är på
+import "./index.css";
+import App from "./App";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contacts from "./routes/Contacts";
+import Blog from "./routes/Blog";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/blog" element={<Blog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
